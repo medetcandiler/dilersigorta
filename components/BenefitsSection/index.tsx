1f8fae0e7a2cd887cb67ee7d7d40f8876b3f9555
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-01 21:42:22 +0300

    Updated

--- components/BenefitsSection/index.tsx
+++ components/BenefitsSection/index.tsx
@@ -2,51 +2,55 @@ import { FC } from "react";
 
 const Benefits: FC = () => {
   return (
-    <div className="container mx-auto ">
-      <div className="flex flex-col items-center gap-5 md:flex-row">
+    <div className="container mx-auto px-5 py-20 ">
+      <div className="flex flex-col justify-center items-center gap-5 md:flex-row">
         <div className="flex flex-col w-1/3">
-          <div className="flex flex-col  px-10 py-9 w-full bg-white shadow-2xl max-md:px-5 max-md:mt-8">
+          <div className="flex flex-col px-10 py-12 w-full bg-white shadow-2xl flex-grow">
             <img
               loading="lazy"
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/09613f0f81c0282f48d09f0e89bda8a36bb0f25a965f696df77098b2149a9d0c?"
               className="object-center w-12 aspect-[1.2]"
             />
-            <div className="mt-2.5 text-2xl font-bold tracking-normal leading-8 text-slate-800">
+            <div className="mt-2.5 text-2xl font-bold tracking-normal text-slate-800">
               Kapsamlı Güvence
             </div>
-            <div className="mt-2.5 text-sm font-medium tracking-wide leading-5 text-neutral-500">
+            <div className="mt-2.5 text-sm font-medium tracking-wide text-neutral-500">
               Ev hırsızlığından doğal afetlere kadar geniş kapsamlı sigorta
               hizmetleri.
             </div>
           </div>
         </div>
+        
         <div className="flex flex-col w-1/3">
-          <div className="flex flex-col grow items-stretch px-10 py-9 w-full bg-white shadow-lg max-md:px-5 max-md:mt-8">
+          <div className="flex flex-col px-10 py-12 w-full bg-white shadow-2xl flex-grow">
             <img
               loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/bbc842d697e57bb51406aab0fb9d7f2f4008fbe94f65da17d46089c6fc25987e?"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/09613f0f81c0282f48d09f0e89bda8a36bb0f25a965f696df77098b2149a9d0c?"
               className="object-center w-12 aspect-[1.2]"
             />
-            <div className="mt-2.5 text-2xl font-bold tracking-normal leading-8 text-slate-800">
-              Geleceği Garantileyin
+            <div className="mt-2.5 text-2xl font-bold tracking-normal text-slate-800">
+              Kapsamlı Güvence
             </div>
-            <div className="mt-2.5 text-sm font-medium tracking-wide leading-5 text-neutral-500">
-              Sigorta poliçelerimizle geleceğinizi güvence altına alın.
+            <div className="mt-2.5 text-sm font-medium tracking-wide text-neutral-500">
+              Ev hırsızlığından doğal afetlere kadar geniş kapsamlı sigorta
+              hizmetleri.
             </div>
           </div>
         </div>
+
         <div className="flex flex-col w-1/3">
-          <div className="flex flex-col  px-10 py-9 w-full text-white bg-sky-500 shadow-lg max-md:px-5 max-md:mt-8">
+          <div className="flex flex-col px-10 py-12 w-full bg-white shadow-2xl flex-grow">
             <img
               loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/40ae4384bcff05b56d921e783bff628f978a796aa01ee56d3f8633853379fa9a?"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/09613f0f81c0282f48d09f0e89bda8a36bb0f25a965f696df77098b2149a9d0c?"
               className="object-center w-12 aspect-[1.2]"
             />
-            <div className="mt-2.5 text-2xl font-bold tracking-normal leading-8 whitespace-nowrap">
-              %100 Müşteri Memnuniyeti
+            <div className="mt-2.5 text-2xl font-bold tracking-normal text-slate-800">
+              Kapsamlı Güvence
             </div>
-            <div className="mt-2.5 text-sm font-medium tracking-wide leading-5">
-              Müşteri memnuniyeti odaklı hizmetimizle her zaman yanınızdayız.
+            <div className="mt-2.5 text-sm font-medium tracking-wide text-neutral-500">
+              Ev hırsızlığından doğal afetlere kadar geniş kapsamlı sigorta
+              hizmetleri.
             </div>
           </div>
         </div>
