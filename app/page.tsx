f5ce65a1833f3cc34455cf9730a4b7ad5e493583
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-01 18:41:20 +0300

    Implemented Benefits section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import Benefits from "@/components/BenefitsSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Benefits />
     </main>
   );
 }
