66080b18e0bfba83002863bfb51f584d9e41dfbe
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-02 17:45:18 +0300

    Updated Benefits section padding x value

--- components/BenefitsSection/index.tsx
+++ components/BenefitsSection/index.tsx
@@ -4,7 +4,7 @@ import { benefits } from "./data";
 
 const Benefits: FC = () => {
   return (
-    <section className="container mx-auto bg-[#ffffff] px-20 py-20 md:px-5">
+    <section className="container mx-auto bg-[#ffffff] px-10 py-20 md:px-5">
       <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
         {benefits.map((benefit) => (
           <BenefitCard
