6fdd15cabdca53d09a82a558fdf282c3b1c60fb4
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-02 21:08:15 +0300

    Improved brandlogo | Updated metadata

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,23 +1,25 @@
 import { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import HamburgerMenu from "../HamburgerMenu";
 import DesktopNav from "../DesktopNav";
 import Arrow from "../Arrow";
+
 
 const Header: FC = () => {
   return (
     <header className="fixed w-full transition duration-300 z-50 bg-[#fff]">
       <section className="relative px-4 flex bg-transparent justify-between items-center h-[70px] md:container md:mx-auto">
         <div className="flex items-center justify-between w-full">
-          <div className="">
+          <Link href='/' className="cursor-pointer">
             <Image
-              src="/images/logolong.png"
+              src="/images/dilerlogo.png"
               alt="logo"
               width={150}
               height={150}
               priority
             />
-          </div>
+          </Link>
 
           <nav className="flex gap-3 md:gap-6">
             <HamburgerMenu />
