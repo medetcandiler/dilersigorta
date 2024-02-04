0a4817513bf0da8933f53d06ad922de72af3f0dc
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-04 17:35:13 +0300

    Updated

--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 const ProductCard: FC<IProductCard> = ({ title, imageSrc }) => {
   return (
-    <Link href='/' className="flex flex-col items-center links-hover">
-      <div className="flex flex-col gap-2 items-center justify-center drop-shadow-2xl px-12 py-6 rounded-xl bg-[#fefefe] h-40 w-60 lg:h-44 lg:w-80">
+    <Link href="/" className="flex flex-col items-center links-hover">
+      <div className="flex flex-col gap-2 items-center justify-center drop-shadow-2xl px-12 py-6 rounded-xl bg-[#fefefe] size-36 sm:size-44 lg:size-56">
         <Image src={imageSrc} alt={title} width={100} height={100} />
         <h3 className="text-center font-semibold ">{title}</h3>
       </div>
--- components/ProductsSection/index.tsx
+++ components/ProductsSection/index.tsx
@@ -2,15 +2,16 @@ import { FC } from "react";
 import ProductCard from "../ProductCard";
 import { products } from "@/data/products";
 
-
 const ProductsSection: FC = () => {
   return (
-    <section className="bg-[#fafafa] px-10 py-20 md:px-5">
-      <h3 className="text-center font-bold text-2xl md:text-3xl lg:text-4xl pb-8">Güvenin Adresi, Sigortada İhtiyacınız Olan Her Şey!</h3>
+    <section className="bg-[#fafafa] px-10 py-20 lg:px-40">
+      <h3 className="text-center font-bold text-2xl md:text-3xl lg:text-4xl pb-8">
+        Güvenin Adresi, Sigortada İhtiyacınız Olan Her Şey!
+      </h3>
       <p className="text-center text-lg pb-6 text-neutral-500">
         Doğru Ürün. İyi Fiyat.
       </p>
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-5 md:grid-cols-2 lg:grid-cols-3">
+      <div className="container mx-auto grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-5 xl:gap-10">
         {products.map((product) => (
           <ProductCard
             key={product.id}
