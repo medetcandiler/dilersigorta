e6010cb6b7a13430dc3c5546096724c2220f74bb
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-04 19:45:40 +0300

    Fixed PartnerSection align issues

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer: FC = () => {
   return (
@@ -122,7 +123,7 @@ const Footer: FC = () => {
             © 2024 Diler Sigorta. Tüm hakları saklıdır.
           </p>
           <div className="flex items-center justify-center gap-5">
-            <a href="">
+            <Link href="https://www.instagram.com/" target="_blank">
               <svg
                 width="24"
                 height="24"
@@ -131,14 +132,14 @@ const Footer: FC = () => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M23.5 11.5704C23.5 5.1801 18.3515 0 12.0014 0C5.64848 0.00143732 0.5 5.1801 0.5 11.5719C0.5 17.3456 4.7056 22.1319 10.2019 23V14.9151H7.28415V11.5719H10.2048V9.02062C10.2048 6.12155 11.9224 4.52037 14.5484 4.52037C15.8075 4.52037 17.1226 4.74603 17.1226 4.74603V7.59193H15.6724C14.2451 7.59193 13.7995 8.4845 13.7995 9.40008V11.5704H16.9875L16.4787 14.9136H13.7981V22.9986C19.2944 22.1304 23.5 17.3441 23.5 11.5704Z"
                   fill="#23A6F0"
                 />
               </svg>
-            </a>
-            <a>
+            </Link>
+            <Link href="https://www.instagram.com/" target="_blank">
               <svg
                 width="24"
                 height="24"
@@ -151,8 +152,8 @@ const Footer: FC = () => {
                   fill="#23A6F0"
                 />
               </svg>
-            </a>
-            <a>
+            </Link>
+            <Link href="https://www.instagram.com/" target="_blank">
               <svg
                 width="24"
                 height="20"
@@ -165,7 +166,7 @@ const Footer: FC = () => {
                   fill="#23A6F0"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
