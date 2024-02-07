b21e9240a29e834930cd1d6a0f5602bb8278e2e0
Medetcan Diler <medetdiler1996@gmail.com>
2024-02-07 15:33:38 +0300

    Implemented goToTopButton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import { Montserrat } from 'next/font/google'
+import "./globals.css";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import GoToTopButton from "@/components/GoToTopButton";
 
 const montserrat = Montserrat({
   weight: '400',
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <Header />
         {children}
+        {true && <GoToTopButton isOpened={true} />}
         <Footer />
       </body>
     </html>
---
+++ components/GoToTopButton/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { FC, MouseEvent, useState, useEffect } from "react";
+import Image from "next/image";
+
+const GoToTopButton: FC = () => {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+
+    if (scrolled > window.innerHeight) {
+      setIsOpened(true);
+    } else if (scrolled <= 300) {
+      setIsOpened(false);
+    }
+  };
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", toggleVisible);
+  }
+
+  const handleClick = (e: MouseEvent<HTMLImageElement>) => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Image
+      onClick={handleClick}
+      className={`fixed  px-3 py-2 rounded-full transition duration-500 ease-out z-50 bottom-6 right-1 md:right-4 cursor-pointer hover:-translate-y-[3px] hover:opacity-85 ${
+        isOpened ? "translate-y-0 opacity-1" : "-translate-y-[100vh] opacity-0"
+      } `}
+      src="/icons/arrowUp.svg"
+      width={65}
+      height={65}
+      alt="go to top button"
+    ></Image>
+  );
+};
+
+export default GoToTopButton;
