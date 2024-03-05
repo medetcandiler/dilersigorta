d41f6e5d096bf098bff44f5510b97a27017787b8
Medetcan Diler <medetdiler1996@gmail.com>
2024-03-05 15:50:28 +0300

    Fixed issue on landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className=" ">
       <HeroSection />
-      <ProductsSection />
+      <ProductsSection isNav={false} />
       <Benefits />
       <InsuranceServicesSection />
       <Testimonials />
